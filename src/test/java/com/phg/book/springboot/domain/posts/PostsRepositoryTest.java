8b7da64e94d8a75c94280a3f224299ed4163c7e7
park hyeong gil <gudrlf7913@naver.com>
2020-08-24 16:20:05 +0900

    Spring Data Jpa Auditing 사용해보기

--- src/test/java/com/phg/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/phg/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -34,5 +36,25 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         Assertions.assertThat(posts.getTitle()).isEqualTo(title);
         Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_register(){
+
+        LocalDateTime now = LocalDateTime.of(2020,8,24,0,0,0);
+        postsRepository.save(Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                        .build());
+
+        List<Posts> postsList = postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createdDate=" + posts.getCreatedDate()+", modifiedDate" + posts.getModifiedDate());
+
+        //Assertions.assertThat(posts.getCreatedDate()).isAfter(LocalDate.now());
+        Assertions.assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
