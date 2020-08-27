1b1affbf8173db6b46408417d997edd4796ecab5
park hyeong gil <gudrlf7913@naver.com>
2020-08-28 00:02:03 +0900

    mustache 템플릿 엔진 사용해보기

---
+++ src/main/java/com/phg/book/springboot/web/IndexController.java
@@ -0,0 +1,18 @@
+package com.phg.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping
+    public String indx(){
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+}
