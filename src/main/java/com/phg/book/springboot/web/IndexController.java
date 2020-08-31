2c2c071ca2d11d323236f8c1e40c220080974c33
park hyeong gil <gudrlf7913@naver.com>
2020-08-31 22:54:01 +0900

    수정, 삭제하는 코드 추가

--- src/main/java/com/phg/book/springboot/web/IndexController.java
+++ src/main/java/com/phg/book/springboot/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.phg.book.springboot.web;
 
+import com.phg.book.springboot.domain.posts.Posts;
 import com.phg.book.springboot.service.posts.PostsService;
+import com.phg.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,5 +29,14 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
