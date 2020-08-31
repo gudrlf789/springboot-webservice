6dab5e6300ced9284807511b1eb85f93e53ec237
park hyeong gil <gudrlf7913@naver.com>
2020-08-31 22:29:19 +0900

    전체 조회 화면 및 controller, service , repository 코드 수정, PostsListResponseDto 파일 새로 생성

--- src/main/java/com/phg/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/phg/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.phg.book.springboot.service.posts;
 
 import com.phg.book.springboot.domain.posts.Posts;
 import com.phg.book.springboot.domain.posts.PostsRepository;
+import com.phg.book.springboot.web.dto.PostsListResponseDto;
 import com.phg.book.springboot.web.dto.PostsResponseDto;
 import com.phg.book.springboot.web.dto.PostsSaveRequestDto;
 import com.phg.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -34,5 +38,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
--- src/main/java/com/phg/book/springboot/web/IndexController.java
+++ src/main/java/com/phg/book/springboot/web/IndexController.java
@@ -1,10 +1,16 @@
 package com.phg.book.springboot.web;
 
+import com.phg.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
 
     @GetMapping
     public String indx(){
@@ -14,5 +20,11 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 }
